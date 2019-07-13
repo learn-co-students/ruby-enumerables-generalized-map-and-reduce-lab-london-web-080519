c10bc481d68c5f53e5456e0c44d4f9ff6b5037fc
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-13 13:11:48 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,13 @@
 # Your Code Here
+
+def map(source_array)
+  source_array.map { |element|
+    yield(element)
+  }
+end
+
+def reduce(source_array, starting_point = 0)
+  source_array.reduce(starting_point) { |sum, element|
+    yield(sum, element)
+  }
+end
