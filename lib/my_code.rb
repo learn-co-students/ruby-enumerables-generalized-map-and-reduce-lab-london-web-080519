f63160d098d37168d9868c4fe91ac88311b728d6
Oliver Burt <oliburt@gmail.com>
2019-07-19 13:11:13 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
 # Your Code Here
+def map(array)
+  new = []
+  array.length.times do |i|
+    new << yield(array[i])
+  end
+  new
+end
+
+def reduce(array, starting_point = nil)
+  if starting_point
+    accumulator = starting_point
+    index = 0
+  else
+    accumulator = array[0]
+    index = 1
+  end
+  while index < array.length do
+    accumulator = yield(accumulator, array[index])
+    index += 1
+  end
+  accumulator
+end
