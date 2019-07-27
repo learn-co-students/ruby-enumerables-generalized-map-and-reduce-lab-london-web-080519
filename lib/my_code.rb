67c8211da684fd804303c9df46733538a130f270
George Kirby <georgeskirby@btinternet.com>
2019-07-27 17:41:24 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
 # Your Code Here
+def map(source_array)
+  new_array = []
+  
+  for i in source_array
+    new_i = yield(i)
+    new_array.push(new_i)   
+  end
+  
+  new_array
+end
+
+def reduce(source_array, starting_point = 0)
+  keep_track = starting_point
+  
+  for i in source_array
+    keep_track = yield(keep_track, i)
+    puts keep_track
+  end
+  
+  keep_track
+end
